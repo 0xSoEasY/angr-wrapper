c088adbf422391f76953835220b70a77c1021fb3
0xSoEasY <julien@rouillier.fr>
2021-07-07 14:16:06 +0200

    Typo in a commentary in win-fail.py

--- win-fail.py
+++ win-fail.py
@@ -1,6 +1,6 @@
 import angr
 
-# Adding 0x400000 because angr map binaries with a base address of 0x400000
+# Adding 0x400000 because angr map PIE binaries with a base address of 0x400000
 win_adress = 0x10BF + 0x400000
 fail_adress = 0x10E3 + 0x400000
 
