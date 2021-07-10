7fce123890e2510bf1343fd576fd0709942430b0
0xSoEasY <julien@rouillier.fr>
2021-07-10 11:17:34 +0200

    Refactoring repo

--- unitary-scripts/string-output.py
+++
@@ -1,7 +0,0 @@
-import angr
-
-proj = angr.Project('../test-binaries/phack-login')
-simgr = proj.factory.simgr()
-simgr.explore(find=lambda s: b"[+] ACCESS GRANTED!" in s.posix.dumps(1))
-
-print(simgr.found[0].posix.dumps(0))
