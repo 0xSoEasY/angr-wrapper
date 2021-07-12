79cb5299290ae4b06064d0df9457867546f38e6e
SoEasY <34216946+0xSoEasY@users.noreply.github.com>
2021-07-12 12:34:38 +0200

    string-output stdin.split(b'\x00')[0]

--- standalone-scripts/string-output.py
+++ standalone-scripts/string-output.py
@@ -4,4 +4,4 @@ proj = angr.Project('../test-binaries/phack-login')
 simgr = proj.factory.simgr()
 simgr.explore(find=lambda s: b"[+] ACCESS GRANTED!" in s.posix.dumps(1))
 
-print(simgr.found[0].posix.dumps(0))
+print(simgr.found[0].posix.dumps(0).split(b'\x00')[0])
